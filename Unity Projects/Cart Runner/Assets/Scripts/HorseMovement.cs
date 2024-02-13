14d986f4c775b2b93c80f04edae237dbaff76c5d
Jacob <jfeathers.03@gamil.com>
2024-02-12 19:52:06 -0700

    redundant data removal

--- Unity Projects/Cart Runner/Assets/Scripts/HorseMovement.cs
+++ Unity Projects/Cart Runner/Assets/Scripts/HorseMovement.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.iOS;
 
 public class HorseMovement : MonoBehaviour
 {
