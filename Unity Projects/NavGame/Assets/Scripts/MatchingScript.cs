a7fd38cb08866856b3f4f7e50e32944be6b269f4
jenriquez6 <victor.admapp01@gmail.com>
2024-03-10 20:20:35 -0600

    Added nav game unity project

---
+++ Unity Projects/NavGame/Assets/Scripts/MatchingScript.cs
@@ -0,0 +1,195 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MatchingScript : MonoBehaviour
+{
+    public string selectedDirection;
+    public string selectedItem;
+
+    public GameObject directionParent;
+    public GameObject findItemsParent;
+
+    public GameObject stopParent;
+
+    [SerializeField]
+    public GameObject[] levels;
+    public int levelCounter;
+
+    public GameObject nextLevelButton;
+
+    public Animator transition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedDirection = "xd";
+        selectedItem = "xdd";
+        levelCounter = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkDirection();
+        checkItems();
+        
+        if (selectedDirection == selectedItem)
+        {
+            itemDone(selectedItem);
+
+            if (stopParent.activeSelf ==true && stopParent.transform.childCount == 3)
+            {
+                refreshDirection();
+
+                nextLevelButton.SetActive(true);
+
+            }
+
+        }
+    }
+
+    public void checkDirection()
+    {
+        Transform parentTransform = directionParent.transform; // Change this to the parent GameObject's transform
+        int childCount = parentTransform.childCount;
+
+        // Iterate through each child
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = parentTransform.GetChild(i);
+
+            // Check if the child has any children
+            int grandchildCount = child.childCount;
+
+            // Iterate through each grandchild
+            for (int j = 0; j < grandchildCount; j++)
+            {
+                Transform grandchild = child.GetChild(j);
+
+                // Check if the grandchild is active and deactivate it if true
+                if (grandchild.gameObject.activeSelf)
+                {
+                    selectedDirection = grandchild.parent.tag;
+
+                }
+            }
+        }
+    }
+
+    public void checkItems()
+    {
+        Transform parentTransform = findItemsParent.transform; // Change this to the parent GameObject's transform
+        int childCount = parentTransform.childCount;
+
+        // Iterate through each child
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = parentTransform.GetChild(i);
+
+            // Check if the child has any children
+            int grandchildCount = child.childCount;
+
+            // Iterate through each grandchild
+            for (int j = 0; j < grandchildCount; j++)
+            {
+                Transform grandchild = child.GetChild(j);
+
+                // Check if the grandchild is active and deactivate it if true
+                if (grandchild.gameObject.activeSelf)
+                {
+                    selectedItem = grandchild.parent.tag;
+
+                }
+            }
+        }
+
+        
+    }
+
+    public void itemDone(string itemTag)
+    {
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(itemTag);
+
+        // Iterate through each GameObject
+        foreach (GameObject obj in taggedObjects)
+        {
+
+            if (obj.transform.parent == findItemsParent.transform)
+            {
+                obj.transform.SetParent(stopParent.transform);
+            }
+            
+
+            // Try to get the Button component from the GameObject
+            Button button = obj.GetComponent<Button>();
+
+            // If the Button component exists, make it uninteractable
+            if (button != null)
+            {
+                button.interactable = false;
+            
+            }
+            else
+            {
+                Debug.LogWarning("No Button component found on: " + obj.name);
+            }
+        }
+    }
+
+    public void refreshDirection()
+    {
+
+        Transform parentTransform = directionParent.transform; // Change this to the parent GameObject's transform
+        int childCount = parentTransform.childCount;
+
+        // Iterate through each child
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = parentTransform.GetChild(i);
+
+            // Try to get the Button component from the GameObject
+            Button button = child.GetComponent<Button>();
+
+            // If the Button component exists, make it uninteractable
+            if (button != null)
+            {
+                button.interactable = true;
+
+            }
+            else
+            {
+                Debug.LogWarning("No Button component found on");
+            }
+        }
+
+    }
+
+    public void goNextLevel()
+    {
+        transition.SetTrigger("Start");
+
+        levels[levelCounter - 1].SetActive(false);
+        Scrolling.panelNumber = 2;
+
+        if(levelCounter <3)
+        {
+            levelCounter += 1;
+            Debug.Log(levelCounter);
+            levels[levelCounter - 1].SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+        
+
+        nextLevelButton.SetActive(false);
+    }
+
+
+
+}
---
+++ Unity Projects/NavGame/Assets/Scripts/Scrolling.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    public static int panelNumber;
+
+    public GameObject currentLevel;
+    public GameObject levelsParent;
+
+    public GameObject leftTransform;
+    public GameObject centerTransform;
+    public GameObject rightTransform;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelNumber = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform child in levelsParent.transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                currentLevel = child.gameObject; // Return the first active child
+            }
+        }
+
+        switch (panelNumber)
+        {
+            case 1:
+                currentLevel.transform.position = leftTransform.transform.position;
+                break;
+
+            case 2:
+                currentLevel.transform.position = centerTransform.transform.position;
+                break;
+
+            case 3:
+                currentLevel.transform.position = rightTransform.transform.position;
+                break;
+        }
+    }
+
+    public void ScrollRight()
+    {
+        if (panelNumber > 1)
+        {
+            panelNumber--;
+        }
+        
+        
+    }
+
+    public void ScrollLeft()
+    {
+
+        if (panelNumber < 3)
+        {
+            panelNumber++;
+        }
+    }
+
+}
