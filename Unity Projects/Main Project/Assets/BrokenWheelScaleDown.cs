f324b49d25f364ea74943445792f20c1493b3bcb
jenriquez6 <victor.admapp01@gmail.com>
2024-03-19 14:49:14 -0600

    adding cartrepair to main

---
+++ Unity Projects/Main Project/Assets/BrokenWheelScaleDown.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenWheelScaleDown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(transform.IsChildOf())
+        //WheelBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+    }
+}
---
+++ Unity Projects/Main Project/Assets/CartRepair/Scripts/CorrectBoxManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CorrectBoxManager : MonoBehaviour
+{
+    [SerializeField] GameObject WheelBox;
+    [SerializeField] GameObject JointBox;
+    [SerializeField] GameObject PinBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (WheelBox.transform.childCount > 0)
+        {
+            //WheelBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(5.4f, 5.4f, 5.4f);
+            WheelBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+            WheelBox.transform.GetChild(0).gameObject.transform.GetComponent<Image>().raycastTarget = false;
+            JointBox.SetActive(true);
+
+
+            if (JointBox.transform.childCount > 0)
+            {
+                //JointBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+                PinBox.SetActive(true);
+
+                if (PinBox.transform.childCount > 0)
+                {
+                    //PinBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+
+
+                }
+            }
+        }
+    }
+}
