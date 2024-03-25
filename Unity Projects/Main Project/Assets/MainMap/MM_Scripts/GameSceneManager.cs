700931400ec218a669d6b5df334595537600bfd7
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2024-03-25 04:06:49 -0600

    Dummy Maps
    This is only to make sure that I can get this working for everyone once they finish their minigames

--- Unity Projects/Main Project/Assets/MainMap/MM_Scripts/GameSceneManager.cs
+++ Unity Projects/Main Project/Assets/MainMap/MM_Scripts/GameSceneManager.cs
@@ -6,9 +6,13 @@ using UnityEngine.SceneManagement;
 public class GameSceneManager : MonoBehaviour
 {
     public void LoadMapScene() { SceneManager.LoadScene(1); }
-    public void LoadPackingScene() { SceneManager.LoadScene(2); }
-    public void LoadBalanceBoatScene() { SceneManager.LoadScene(3); }
-    public void LoadCartRepairScene() { SceneManager.LoadScene(4); }
-    public void LoadEndingScene() { SceneManager.LoadScene(5); }
-    public void LoadDummy() { SceneManager.LoadScene(2); }
+    //public void LoadBalanceBoatScene() { SceneManager.LoadScene(3); }
+    //public void LoadCartRepairScene() { SceneManager.LoadScene(4); }
+    //public void LoadEndingScene() { SceneManager.LoadScene(5); }
+    //public void LoadDummy() { SceneManager.LoadScene(2); }
+
+    public void LoadGameScene(int currentProgress)
+    {
+        SceneManager.LoadScene(currentProgress + 1);
+    }
 }
