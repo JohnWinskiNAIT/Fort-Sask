9aa96a80f2ad6f03e5bfced12438acc2dc3902d0
jenriquez6 <victor.admapp01@gmail.com>
2024-04-04 13:52:55 -0600

    added random wincon to main (cartrepair)(

--- Unity Projects/Main Project/Assets/CartRepair/Scripts/CorrectBoxManager.cs
+++ Unity Projects/Main Project/Assets/CartRepair/Scripts/CorrectBoxManager.cs
@@ -9,12 +9,12 @@ public class CorrectBoxManager : MonoBehaviour
     [SerializeField] GameObject WheelBox;
     [SerializeField] GameObject JointBox;
     [SerializeField] GameObject PinBox;
-    [SerializeField] GameObject GameWin;
+   //public static GameObject GameWin;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //GameWin = GameObject.Find("nextLevel");
     }
 
     // Update is called once per frame
@@ -22,8 +22,8 @@ public class CorrectBoxManager : MonoBehaviour
     {
         if (WheelBox.transform.childCount > 0)
         {
-            //WheelBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(5.4f, 5.4f, 5.4f);
-            WheelBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+            ////WheelBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(5.4f, 5.4f, 5.4f);
+            WheelBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             WheelBox.transform.GetChild(0).gameObject.transform.GetComponent<Image>().raycastTarget = false;
             JointBox.SetActive(true);
 
@@ -36,7 +36,7 @@ public class CorrectBoxManager : MonoBehaviour
                 if (PinBox.transform.childCount > 0)
                 {
                     //PinBox.transform.GetChild(0).gameObject.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
-                    GameWin.SetActive(true);
+                    //GameWin.SetActive(true);
 
                 }
             }
---
+++ Unity Projects/Main Project/Assets/CartRepair/Scripts/GameStart.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    [SerializeField]
+    GameObject startBoxes;
+
+    [SerializeField]
+    GameObject[] cartItems;
+
+    [SerializeField]
+    Sprite[] brokenCartItems;
+
+    GameObject brokenTag;
+    GameObject CorrectBoxes;
+
+    [SerializeField]
+    GameObject nextLevel;
+
+    int winCon = 2;
+
+    private bool winConPicked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startBoxes.SetActive(false);
+        brokenTag = GameObject.Find("BrokenTag");
+        CorrectBoxes = GameObject.Find("CorrectBoxes");
+        GameObject.Find("WheelPin").GetComponent<Image>().raycastTarget = false;
+
+        generateBrokenPart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for deactivating start boxes after something is picked
+        if (startBoxes.activeSelf)
+        {
+            pickedWinCon(GameObject.Find("ItemBoxes").transform);
+
+            // If the grandchild is found, do something
+            if (winConPicked)
+            {
+                startBoxes.SetActive(false);
+                gameObject.GetComponent<Button>().enabled = false;
+                GameObject.Find("WheelPin").GetComponent<Image>().raycastTarget = true;
+            }
+        }
+
+        if (GameObject.Find("CorrectBoxes").transform.GetChild(1).gameObject.transform.childCount > 0)
+        {
+            checkWinCon();
+        }
+
+        
+    }
+
+    public void generateBrokenPart()
+    {
+        int partIndex = Random.Range(1, 4);
+        GameObject whichPart = GameObject.Find("StartParent").transform.GetChild(partIndex - 1).gameObject;
+
+        Debug.Log(partIndex);
+
+        whichPart.GetComponent<Image>().sprite = brokenCartItems[partIndex - 1];
+
+        brokenTag.transform.SetParent(whichPart.transform);
+    }
+
+    public void openStartBoxes()
+    {
+        if (!startBoxes.activeSelf)
+        {
+            startBoxes.SetActive(true);
+        }
+        else
+        {
+            startBoxes.SetActive(false);
+        }
+
+
+    }
+
+    void pickedWinCon(Transform parent)
+    {
+        // Iterate through each child of the parent
+        foreach (Transform child in parent)
+        {
+            // If the child has children, recursively search through them
+            if (child.childCount > 0)
+            {
+                winConPicked = true;
+            }
+        }
+    }
+
+    void checkWinCon()
+    {
+
+        foreach (Transform child in CorrectBoxes.transform)
+        {
+            foreach (Transform grandChild in child)
+            {
+                if (grandChild.childCount > 0)
+                {
+                    winCon = 0;
+                }
+            }
+        }
+
+        showEnd();
+    }
+
+    void showEnd()
+    {
+        if (winCon == 2)
+        {
+            winCon = 1;
+        }
+
+        if (winCon == 1)
+        {
+            nextLevel.SetActive(true);
+        }
+        else if (winCon == 0)
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+
+    int CountGrandchildren(Transform parent)
+    {
+        int count = 0;
+        foreach (Transform child in parent)
+        {
+            foreach (Transform grandchild in child)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
