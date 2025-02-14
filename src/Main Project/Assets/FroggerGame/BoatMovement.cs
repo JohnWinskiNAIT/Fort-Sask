2cdb93b8974ed87737eea5531bfa785a890f4694
sncallag <scallaghan3@nait.ca>
2025-02-13 17:27:14 -0700

    FroggerMovement

---
+++ src/Main Project/Assets/FroggerGame/BoatMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    [SerializeField] private float moveDistance = 1f;
+    [SerializeField] private float moveCoolDown = 0.2f;
+
+    private Rigidbody2D rb;
+    private float lastMoveTime;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Move(Vector2 direction)
+    {
+        if (Time.time - lastMoveTime < moveCoolDown) return;
+        rb.MovePosition(rb.position + direction * moveDistance);
+
+        lastMoveTime = Time.time;
+
+    }
+
+    public void MoveUp()
+    {
+        Move(Vector2.up);
+    }
+
+    public void MoveLeft()
+    {
+        Move(Vector2.left);
+    }
+
+    public void MoveRight()
+    {
+        Move(Vector2.right);
+    }
+}
