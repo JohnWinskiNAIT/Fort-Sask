21ddc5e3e5125223c01bfdcfbe3356d7a4c472e6
Michael <mtaylor62@nait.ca>
2025-03-27 13:54:42 -0600

    Update Game 5 with new objects
    All variations are now objects to avoid in Game 5.
    
    Added new assets to Game 2 & 5.
    
    Added 4 new prefabs with the new art assets included.

--- src/Main Project/Assets/Scenes/Frogger Content/LogSpawn.cs
+++ src/Main Project/Assets/Scenes/Frogger Content/LogSpawn.cs
@@ -8,7 +8,25 @@ public class LogSpawn : MonoBehaviour
     public GameObject log;
 
     [SerializeField]
+    public GameObject log2;
+
+    [SerializeField]
+    public GameObject log3;
+
+    [SerializeField]
     public GameObject crate;
+
+    [SerializeField]
+    public GameObject crate2;
+
+    [SerializeField]
+    public GameObject crate3;
+
+    //[SerializeField]
+    //public GameObject brokenBoat;
+
+    //[SerializeField]
+    //public GameObject whirlPool;
 
     [SerializeField]
     public Transform[] spawnPoints;
@@ -35,15 +53,46 @@ public class LogSpawn : MonoBehaviour
         //Instantiate(log, spawnPoint.position, spawnPoint.rotation);
 
         //randomize what object spawns
-        int trashspwner = Random.Range(1, 3);
+        int trashspwner = Random.Range(1, 7);
         if (trashspwner == 1)
         {
             Instantiate(log, spawnPoint.position, spawnPoint.rotation);
         }
+
         if (trashspwner == 2)
+        {
+            Instantiate(log2, spawnPoint.position, spawnPoint.rotation);
+        }
+
+        if (trashspwner == 3)
+        {
+            Instantiate(log3, spawnPoint.position, spawnPoint.rotation);
+        }
+
+        if (trashspwner == 4)
         {
             Instantiate(crate, spawnPoint.position, spawnPoint.rotation);
         }
+
+        if (trashspwner == 5)
+        {
+            Instantiate(crate2, spawnPoint.position, spawnPoint.rotation);
+        }
+
+        if (trashspwner == 6)
+        {
+            Instantiate(crate3, spawnPoint.position, spawnPoint.rotation);
+        }
+
+        //if (trashspwner == 7)
+        //{
+        //    Instantiate(brokenBoat, spawnPoint.position, spawnPoint.rotation);
+        //}
+
+        //if (trashspwner == 8)
+        //{
+        //    Instantiate(whirlPool, spawnPoint.position, spawnPoint.rotation);
+        //}
     }
 
    
