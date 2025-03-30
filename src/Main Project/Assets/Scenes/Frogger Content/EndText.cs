c939387de326a839aff9966abb14029c8285ecd7
Michael <mtaylor62@nait.ca>
2025-03-30 09:49:35 -0600

    Added Assets & Fixed Text for Game 5
    Added new assets and Fixed Text for Boat Count when Game 5 is completed.

--- src/Main Project/Assets/Scenes/Frogger Content/EndText.cs
+++ src/Main Project/Assets/Scenes/Frogger Content/EndText.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class EndText : MonoBehaviour
 {
     public TextMeshProUGUI endtext;
+
     // Start is called before the first frame update
     void Start()
     {
-        endtext.text = LifeSystem.Boats.ToString();
+        endtext.text = $"{LifeSystem.Boats}/3 Ferries made it across!";
     }
 
     // Update is called once per frame
